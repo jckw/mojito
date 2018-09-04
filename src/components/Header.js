e8a9ae30140771b847670f67777ac06e20ab59ae
Jack Weatherilt <jackweatherilt@outlook.com>
2018-09-04 12:57:16 +0100

    Add temporary logo.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react'
-import { Flex, Box, Text } from 'rebass'
+import { Flex, Box } from 'rebass'
 import { Link } from 'react-router-dom'
+import Logo from '../assets/Logo.svg'
 
 class Header extends Component {
     render() {
         return (
             <Flex flex-direction="row">
-                <Box>
-                    <Text>
-                        <Link to="/">snapspace</Link>
-                    </Text>
+                <Box p={3} width="200px">
+                    <Link to="/">
+                        <img src={Logo} width="100%" height="auto" />
+                    </Link>
                 </Box>
             </Flex>
         )
