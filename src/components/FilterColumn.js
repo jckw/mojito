ca56fc298452f6aedbe27bdbba8c597c333a4a62
Jack Weatherilt <jackweatherilt@outlook.com>
2018-09-05 21:40:24 +0100

    Add help boxes.

--- src/components/FilterColumn.js
+++ src/components/FilterColumn.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
-import { Container, Card, Flex, Box, Text } from 'rebass'
+import { Container, Card, Flex, Box, Text, Heading } from 'rebass'
 
 import people from '../assets/icons/people.svg'
 import money from '../assets/icons/money.svg'
 import cyclist from '../assets/icons/cyclist.svg'
+import brain from '../assets/icons/brain.svg'
 import Logo from '../assets/Logo.svg'
 
 const FilterField = ({ icon, children }) => (
@@ -17,19 +18,39 @@ class PropertyColumn extends Component {
     render() {
         return (
             <Container maxWidth={['200px']} width="100%" bg="green.pale">
-                <Box my={3} mx={2}>
-                    <img src={Logo} style={{ height: '24px' }} />
-                </Box>
-                <Card bg="green.grey" p={3} pb={0} mb={3}>
-                    <FilterField icon={money} />
-                    <FilterField icon={people} />
-                    <FilterField icon={cyclist} />
-                </Card>
-                <Card bg="green.grey" p={3} mb={3}>
-                    <Text color="green.dark" fontSize={1}>
-                        Looking for advice on how not to get ripped off by an estate agent?
-                    </Text>
-                </Card>
+                <Flex
+                    flexDirection="column"
+                    justifyContent="space-between"
+                    css={{ height: '100%' }}
+                >
+                    <Box>
+                        <Box my={3} mx={2}>
+                            <img src={Logo} style={{ height: '24px' }} />
+                        </Box>
+                        <Card bg="green.grey" p={3} pb={0} mb={3}>
+                            <FilterField icon={money} />
+                            <FilterField icon={people} />
+                            <FilterField icon={cyclist} />
+                        </Card>
+                        <Card bg="green.grey" p={3} mb={3}>
+                            <Flex justifyContent="space-between" mb={2}>
+                                <Heading color="green.dark" fontSize={2}>
+                                    Wise up about rent.
+                                </Heading>
+                                <img src={brain} style={{ height: '2em' }} />
+                            </Flex>
+                            <Text color="green.dark" fontSize={1}>
+                                What are the fees? What's the average rent for this area? How do I
+                                book a viewing?
+                            </Text>
+                        </Card>
+                    </Box>
+                    <Box my={3}>
+                        <Text fontSize={0} color="green.dark">
+                            Contact
+                        </Text>
+                    </Box>
+                </Flex>
             </Container>
         )
     }
