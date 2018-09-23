6fd06a97d373ec12b0537788bf2dfd79ad58dbb5
Jack Weatherilt <jackweatherilt@outlook.com>
2018-09-23 11:49:48 +0100

    Temporary fix for short photos.

--- src/components/PropertyItem.js
+++ src/components/PropertyItem.js
@@ -46,7 +46,7 @@ class PropertyItem extends Component {
         const photos = photoEdges.edges.map(e => (
             <img
                 key={e.node.photo}
-                style={{ height: 'auto', width: '100%' }}
+                style={{ height: '100%', width: '100%', objectFit: 'cover' }}
                 src={`${BASE_MEDIA_URL}/media/${e.node.photo}`}
             />
         ))
@@ -73,7 +73,7 @@ class PropertyItem extends Component {
                             css={{ height: '180px', overflowY: 'hidden' }}
                         >
                             {photos.length > 0 ? (
-                                <Box>{photos}</Box>
+                                <Box css={{ height: '100%' }}>{photos}</Box>
                             ) : (
                                 <Box css={{ width: '100%', height: '100%' }} bg="grey.0" />
                             )}
