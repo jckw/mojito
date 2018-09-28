2ed1a130db9c7905df23f9474519540e774e374b
Jack Weatherilt <jackweatherilt@outlook.com>
2018-09-28 12:42:19 +0100

    Jazz up the landing page a little bit.

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react'
-import { Box, Heading } from 'rebass'
+import { Flex, Box, Heading, Text } from 'rebass'
 
 class Landing extends Component {
     render() {
         return (
-            <Box>
-                <Heading>movemaison.com makes student properties easy.</Heading>
-            </Box>
+            <Flex
+                css={{ minHeight: '100vh' }}
+                bg="green.normal"
+                color="white"
+                alignItems="center"
+                flexDirection="column"
+                justifyContent="center"
+            >
+                <Box p={4}>
+                    <Heading fontWeight="heavy" fontSize={8} lineHeight="1" mb={2}>
+                        Moving out.
+                        <br />
+                        Made easy.
+                    </Heading>
+                    <Text fontWeight="medium" fontSize={5}>
+                        Find you next flat quickly, with <b>movemaison</b>.
+                    </Text>
+                </Box>
+            </Flex>
         )
     }
 }
