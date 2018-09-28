75426f10bba4d945cd4429b386985c92d318c6f8
Jack Weatherilt <jackweatherilt@outlook.com>
2018-09-28 12:56:44 +0100

    Make bare-bones landing page.

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
-import { Flex, Box, Heading, Text } from 'rebass'
+import { Flex, Box, Heading, Text, Button } from 'rebass'
+import { Link } from 'react-router-dom'
+
+import arrowRight from '../assets/arrowRight.svg'
 
 class Landing extends Component {
     render() {
@@ -12,15 +15,33 @@ class Landing extends Component {
                 flexDirection="column"
                 justifyContent="center"
             >
-                <Box p={4}>
-                    <Heading fontWeight="heavy" fontSize={8} lineHeight="1" mb={2}>
+                <Box p={4} mx="auto" css={{ width: '100%', maxWidth: '500px' }}>
+                    <Heading fontWeight="heavy" fontSize={7} lineHeight="1" mb={1}>
                         Moving out.
                         <br />
                         Made easy.
                     </Heading>
-                    <Text fontWeight="medium" fontSize={5}>
+                    <Text fontWeight="normal" fontSize={4} mb={3}>
                         Find you next flat quickly, with <b>movemaison</b>.
                     </Text>
+                    <Link to="/search/oxford">
+                        <Button
+                            css={{
+                                background: '#1B7957',
+                                boxShadow: '0 2px 20px -6px #1B7957',
+                                width: '100%'
+                            }}
+                            py="24px"
+                            px="30px"
+                            fontWeight="normal"
+                            borderRadius="18px"
+                        >
+                            <Flex justifyContent="space-between" alignItems="center">
+                                <Text>University of Oxford</Text>
+                                <img src={arrowRight} />
+                            </Flex>
+                        </Button>
+                    </Link>
                 </Box>
             </Flex>
         )
